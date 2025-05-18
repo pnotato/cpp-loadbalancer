7961d32ffee99ac1736ef310a657ab9f860b1929
Nicholas Chan <nicholasch.an@outlook.com>
2025-05-17 17:01:06 -0700

    sockets

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <string.h>
 #include <string>
 
+// Basic TCP Server First
+
 int main() {
     // Create a socket
     int listening = socket(AF_INET, SOCK_STREAM, 0);
@@ -20,10 +22,38 @@ int main() {
     // Bind the socket to IP / Port
     sockaddr_in hint;
     hint.sin_family = AF_INET;
-    hint.sin_port = 54000;
+    hint.sin_port = htons(54000); // host to network short
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+
+    if (bind(listening, (sockaddr*)&hint, sizeof(hint)) == -1)
+    {
+        std::cerr <<  "Can't bind to port";
+        return -2;
+    }
 
     // Mark the socket for listening in 
+    if (listen(listening, SOMAXCONN) == -1)
+    {
+        std::cerr << "Can't listen";
+        return -3;
+    }
+
     // Accept a call
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+    char host[NI_MAXHOST];
+    char svc[NI_MAXSERV];
+
+    int clientSocket = accept(listening, (sockaddr*)&client,  &clientSize)
+
+    if (clientSocket == -1)
+    {
+        std::cerr << "Problem with client connecting";
+    }
+
+    close(listening);
+    memset(host, 0, NI_MAXHOST);
+
     // Close the listening socket
     // While recieving -> display message, echo message.
     return 0;
